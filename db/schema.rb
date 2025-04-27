64d1c8d8aa99a269df9263eb11bc61f5f587fc3d
aman <amangarg10569@gmail.com>
2025-04-27 19:38:46 +0530

    [Aman] MOD: Made changes in Production File

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_27_064234) do
     t.float "rating"
     t.integer "duration"
     t.text "description"
-    t.boolean "premium"
+    t.boolean "premium", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
